d9905efc390120bb0e257d04678f3182628aa3ae
Shesh Ghimire <sheshgh@outlook.com>
2025-03-14 10:42:12 +0545

    add: `ScrapeFrom::errorMsg()` method.

--- Src/Attributes/ScrapeFrom.php
+++ Src/Attributes/ScrapeFrom.php
@@ -8,4 +8,8 @@ use Attribute;
 #[Attribute( flags: Attribute::TARGET_CLASS )]
 final readonly class ScrapeFrom {
 	public function __construct( public string $name, public string $url, public string $filename ) {}
+
+	public function errorMsg(): string {
+		return sprintf( 'Error occurred for source: "%1$s" when scaping from URL: "%2$s"', $this->name, $this->url );
+	}
 }
