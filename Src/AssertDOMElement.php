42e475b52c0338b973e0a2ed2adf7a6d3ae1ca96
Shesh Ghimire <sheshgh@outlook.com>
2025-03-18 17:30:51 +0545

    add: `AssertDOMElement::isValid()` to validate node is `DOMElement`.

--- Src/AssertDOMElement.php
+++ Src/AssertDOMElement.php
@@ -3,6 +3,7 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Scraper;
 
+use DOMNode;
 use DOMElement;
 
 class AssertDOMElement {
@@ -12,6 +13,11 @@ class AssertDOMElement {
 
 	public static function hasClass( DOMElement $element, string $classname ): bool {
 		return self::has( $element, $classname, type: 'class' );
+	}
+
+	/** @phpstan-assert-if-true =DOMElement $node */
+	public static function isValid( DOMNode $node, int $childCount = 0 ): bool {
+		return $node instanceof DOMElement && ( ! $childCount || $node->childNodes->length >= $childCount );
 	}
 
 	private static function has( DOMElement $element, string $value, string $type ): bool {
