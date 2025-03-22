15d737c63a506ad130550ca242c320aca17c4761
Shesh Ghimire <sheshgh@outlook.com>
2025-03-22 17:56:47 +0545

    add: support for no-implicit hml tags, method to get body element.

--- Src/DOMDocumentFactory.php
+++ Src/DOMDocumentFactory.php
@@ -3,32 +3,35 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Scraper;
 
+use DOMElement;
 use DOMDocument;
 use TheWebSolver\Codegarage\Scraper\Helper\Normalize;
 use TheWebSolver\Codegarage\Scraper\Error\InvalidSource;
 
 class DOMDocumentFactory {
 	/** @throws InvalidSource When cannot load HTML content to the DOM Document. */
-	public static function createFromHtml( string $contentOrFile ): DOMDocument {
-		$dom     = new DOMDocument();
-		$options = LIBXML_NOERROR | LIBXML_NOBLANKS;
-		$source  = is_readable( $contentOrFile )
+	public static function createFromHtml(
+		string $contentOrFile,
+		bool $noImpliedHtmlBody = true,
+		bool $normalize = true
+	): DOMDocument {
+		$options                        = LIBXML_NOERROR | LIBXML_NOBLANKS;
+		$noImpliedHtmlBody && $options |= LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED;
+		$dom                            = new DOMDocument();
+		$dom->formatOutput              = false;
+		$source                         = is_readable( $contentOrFile )
 			? ( file_get_contents( $contentOrFile ) ?: throw InvalidSource::contentNotFound( $contentOrFile ) )
 			: $contentOrFile;
 
-		self::maybeWithoutHtmlOrBodyElement( $source, $options );
-
-		$dom->formatOutput = false;
-
-		$dom->loadHTML( Normalize::controlsAndWhitespacesIn( $source ), $options )
+		$dom->loadHTML( $normalize ? Normalize::controlsAndWhitespacesIn( $source ) : $source, $options )
 			?: throw InvalidSource::nonLoadableContent();
 
 		return $dom;
 	}
 
-	private static function maybeWithoutHtmlOrBodyElement( string $source, int &$options ): void {
-		if ( ! str_contains( $source, '</html>' ) ) {
-			$options |= LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED;
-		}
+	public static function bodyFromHtml( string $contentOrFile, bool $normalize = true ): DOMElement {
+		return self::createFromHtml( $contentOrFile, noImpliedHtmlBody: false, normalize: $normalize )
+			->getElementsByTagName( 'body' )
+			->item( 0 ) ?? throw InvalidSource::nonLoadableContent();
 	}
 }
--- Tests/DOMDocumentFactoryTest.php
+++ Tests/DOMDocumentFactoryTest.php
@@ -18,8 +18,17 @@ class DOMDocumentFactoryTest extends TestCase {
 
 		$this->assertSame(
 			'div',
-			DOMDocumentFactory::createFromHtml( $content )->firstChild?->nodeName,
-			'Must not create <html> node automatically if source does not have that specific tag'
+			DOMDocumentFactory::createFromHtml( $content, normalize: false )->firstChild?->nodeName,
+			'Does not create <html> & <body> node automatically if source does not have those specific tags'
+		);
+
+		$dom = DOMDocumentFactory::createFromHtml( $content, noImpliedHtmlBody: false, normalize: false );
+
+		$this->assertCount( 2, $dom->childNodes );
+		$this->assertSame(
+			XML_DOCUMENT_TYPE_NODE,
+			$dom->firstChild?->nodeType,
+			'Creates <!DOCTYPE>, <html> and <body> elements automatically $noImpliedHtmlBody is "false"'
 		);
 
 		$content = <<<CONTENT_WITH_HTML_TAG
@@ -30,17 +39,60 @@ class DOMDocumentFactoryTest extends TestCase {
 		</html>
 		CONTENT_WITH_HTML_TAG;
 
-		$this->assertSame( 'html', DOMDocumentFactory::createFromHtml( $content )->firstChild?->nodeName );
+		$this->assertSame(
+			XML_ELEMENT_NODE,
+			DOMDocumentFactory::createFromHtml( $content, normalize: false )->firstChild?->nodeType,
+			'Does not create <!DOCTYPE> node automatically if source does not have that specific tag'
+		);
+
+		$this->assertSame(
+			XML_DOCUMENT_TYPE_NODE,
+			DOMDocumentFactory::createFromHtml( $content, noImpliedHtmlBody: false, normalize: false )->firstChild?->nodeType,
+			'Creates <!DOCTYPE> node automatically $noImpliedHtmlBody is "false"'
+		);
+
+		$partialContent = '
+			<!-- first node is comment -->
+			<nav>Navigation</nav>
+			<div>Content</div>
+		';
+
+		// Captures all three nodes when first node is comment.
+		$this->assertCount( 3, DOMDocumentFactory::createFromHtml( $partialContent )->childNodes );
+
+		$partialContent = '
+			<nav>Navigation</nav>
+			<!-- first node is <nav> element -->
+			<div>Breadcrumbs</div>
+			<!-- another comment -->
+			<main>Main content</main>
+		';
+
+		// Only captures first node and all comment nodes when first node is not comment.
+		// This is how PHP DOMDocument works and is not limitation of DOMDocumentFactory.
+		$this->assertCount( 3, DOMDocumentFactory::createFromHtml( $partialContent )->childNodes );
 	}
 
 	#[Test]
 	public function itCreateDomDocumentBasedOnHTMLfilepath(): void {
 		$dom = DOMDocumentFactory::createFromHtml( self::RESOURCE_PATH . 'partial-content.html' );
 
-		$this->assertSame( 'div', $dom->firstChild?->nodeName );
+		$this->assertSame( 'nav', $dom->firstChild?->nodeName );
 
 		$dom = DOMDocumentFactory::createFromHtml( self::RESOURCE_PATH . 'full-content.html' );
 
-		$this->assertSame( 'html', $dom->firstChild?->nodeName );
+		$this->assertSame( XML_DOCUMENT_TYPE_NODE, $dom->firstChild?->nodeType );
+		$this->assertSame( XML_ELEMENT_NODE, $dom->lastChild?->nodeType );
+	}
+
+	#[Test]
+	public function itReturnsHtmlBodyElement(): void {
+		$fullBody    = DOMDocumentFactory::bodyFromHtml( self::RESOURCE_PATH . 'full-content.html' );
+		$partialBody = DOMDocumentFactory::bodyFromHtml( self::RESOURCE_PATH . 'partial-content.html' );
+
+		$this->assertSame( 2, $fullBody->childNodes->length, 'Includes text node & div element' );
+		$this->assertSame( 1, $fullBody->childElementCount, 'Includes only div element' );
+		$this->assertSame( 3, $partialBody->childElementCount, 'Includes one nav & two divs' );
+		$this->assertSame( 5, $partialBody->childNodes->length, 'Includes one nav, two comments & two divs' );
 	}
 }
