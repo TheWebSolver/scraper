1f8c06230e538583338dbdebf9f71d9f6ded5d2d
Shesh Ghimire <sheshgh@outlook.com>
2025-03-25 13:03:59 +0545

    remove: unused `Normalize::nodesToArray()` method.

--- Src/Helper/Normalize.php
+++ Src/Helper/Normalize.php
@@ -3,24 +3,12 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Scraper\Helper;
 
-use DOMNode;
-use DOMNodeList;
-
 class Normalize {
 	final public const NON_BREAKING_SPACES = array( '&nbsp;', /* "&" entity is "&amp;" + "nbsp;" */ '&amp;nbsp;' );
 	final public const CONTROLS            = array( "\n", "\r", "\t", "\v" );
 
 	public static function nonBreakingSpaceToWhitespace( string $value ): string {
 		return html_entity_decode( str_replace( self::NON_BREAKING_SPACES, ' ', htmlspecialchars( $value ) ) );
-	}
-
-	/**
-	 * @param DOMNodeList<TNode> $nodes
-	 * @return TNode[]
-	 * @template TNode of DOMNode
-	 */
-	public static function nodesToArray( DOMNodeList $nodes ): array {
-		return iterator_to_array( $nodes, preserve_keys: false );
 	}
 
 	public static function controlsAndWhitespacesIn( string $value ): string {
