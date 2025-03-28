4a79b2ea2dbd4a0be09fd74f1542a259a9029dce
Shesh Ghimire <sheshgh@outlook.com>
2025-03-28 15:23:39 +0545

    add: `TableColumnMarshaller::$collectable` to collect only subset of columns.

--- Src/Marshaller/TableColumnMarshaller.php
+++ Src/Marshaller/TableColumnMarshaller.php
@@ -9,8 +9,20 @@ use TheWebSolver\Codegarage\Scraper\Interfaces\Transformer;
 
 /** @template-implements Transformer<string> */
 class TableColumnMarshaller implements Transformer {
+	/** @param list<string> $collectable Subset of column names to be collected. */
+	public function __construct( private readonly array $collectable = array() ) {}
+
 	/** @param TableTracer<mixed,string> $tracer */
 	public function transform( string|DOMElement $element, int $position, TableTracer $tracer ): string {
-		return trim( $element instanceof DOMElement ? $element->textContent : $element );
+		$content = trim( $element instanceof DOMElement ? $element->textContent : $element );
+
+		return $this->columnNameExistsInCollectable( $tracer ) ? $content : '';
+	}
+
+	/** @param TableTracer<mixed,string> $tracer */
+	protected function columnNameExistsInCollectable( TableTracer $tracer ): bool {
+		return ! $this->collectable
+			|| ! ( $columnName = $tracer->getCurrentColumnName() )
+			|| in_array( $columnName, $this->collectable, strict: true );
 	}
 }
