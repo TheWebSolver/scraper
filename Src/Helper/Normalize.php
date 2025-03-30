cbc2d3cad733f7c981b977bc80843eb5bca0d023
Shesh Ghimire <sheshgh@outlook.com>
2025-03-30 16:08:58 +0545

    new: `Normalize::listWithOffset()` to re-calculate given $array index.

--- Src/Helper/Normalize.php
+++ Src/Helper/Normalize.php
@@ -3,6 +3,8 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Scraper\Helper;
 
+use TheWebSolver\Codegarage\Scraper\Error\InvalidSource;
+
 class Normalize {
 	final public const NON_BREAKING_SPACES = array( '&nbsp;', /* "&" entity is "&amp;" + "nbsp;" */ '&amp;nbsp;' );
 	final public const CONTROLS            = array( "\n", "\r", "\t", "\v" );
@@ -31,5 +33,39 @@ class Normalize {
 		);
 
 		return trim( $withoutWhitespaceBetweenTags ?? $toSingleWhitespace ?? $withoutControls );
+	}
+
+	/**
+	 * @param list<TValue>   $array  Eg: **['one', 'three', 'five']**
+	 * @param array<int,int> $offset Eg: **[0, 2, 4]**
+	 *
+	 * @return array{0:array<int,TValue>,1:array<int,int>,2:int} Returns:
+	 * - **0:** Resulted array -> `[1=>'one', 3=>'three', 5=>'five']`
+	 * - **1:** Flipped offset -> `[0=>0, 2=>1, 4=>2]`
+	 * - **2:** Last index key -> `5` of the resulted array
+	 *
+	 * @throws InvalidSource When last offset index not found even if non-empty $offset given.
+	 *
+	 * @template TValue
+	 */
+	public static function listWithOffset( array $array, array $offset ): array {
+		if ( empty( $offset ) ) {
+			return array( $array, array(), array_key_last( $array ) ?? 0 );
+		}
+
+		$lastOffset = end( $offset ) ?? throw new InvalidSource( 'Last offset index not found' );
+		$offset     = array_flip( $offset );
+		$result     = array();
+		$current    = 0;
+
+		for ( $i = 0; $i <= $lastOffset; $i++ ) {
+			isset( $offset[ $i ] ) || ( $result[ $i ] = $array[ $current++ ] );
+		}
+
+		foreach ( $array as $value ) {
+			in_array( $value, $result, strict: true ) || ( $result[ ++$lastOffset ] = $value );
+		}
+
+		return array( $result, $offset, $lastOffset );
 	}
 }
--- Tests/NormalizeTest.php
+++ Tests/NormalizeTest.php
@@ -3,9 +3,10 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Test;
 
-use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\DataProvider;
+use TheWebSolver\Codegarage\Scraper\Enums\Table;
 use TheWebSolver\Codegarage\Scraper\Helper\Normalize;
 
 class NormalizeTest extends TestCase {
@@ -34,6 +35,36 @@ class NormalizeTest extends TestCase {
 										  </b>
 				',
 			),
+		);
+	}
+
+	/**
+	 * @param list<mixed>      $array
+	 * @param array<int,int>   $offset
+	 * @param array<int,mixed> $expected
+	 */
+	#[Test]
+	#[DataProvider( 'provideArrayWithOffsetKeys' )]
+	public function itReIndexesGivenArrayWithOffset( array $array, array $offset, array $expected ): void {
+		[$reindexed, $offsetFlipped, $lastIndex] = Normalize::listWithOffset( $array, $offset );
+
+		$this->assertSame( $expected, $reindexed );
+		$this->assertSame( array_key_last( $expected ), $lastIndex );
+		$this->assertSame( array_flip( $offset ), $offsetFlipped );
+	}
+
+	/** @return mixed[] */
+	// phpcs:disable WordPress.Arrays.ArrayDeclarationSpacing.AssociativeArrayFound
+	public static function provideArrayWithOffsetKeys(): array {
+		return array(
+			array( array( 'without', 'offset' ), array(), array( 0 => 'without', 1 => 'offset' ) ),
+			array( array( '2', '5', '6' ), array( 0, 1, 3, 4 ), array( 2 => '2', 5 => '5', 6 => '6' ) ),
+			array( array( '0', '1', '5' ), array( 2, 3, 4 ), array( 0 => '0', 1 => '1', 5 => '5' ) ),
+			array( array( '0', '3', '4' ), array( 1, 2 ), array( 0 => '0', 3 => '3', 4 => '4' ) ),
+			array( array( '3', '6' ), array( 0, 1, 2, 4, 5 ), array( 3 => '3', 6 => '6' ) ),
+			array( array( '2', '4', '6' ), array( 0, 1, 3, 5 ), array( 2 => '2', 4 => '4', 6 => '6' ) ),
+			array( array( '3', '4', '7' ), array( 0, 1, 2, 5, 6 ), array( 3 => '3', 4 => '4', 7 => '7' ) ),
+			array( array( Table::Row, Table::Column ), array( 0, 1, 2 ), array( 3 => Table::Row, 4 => Table::Column ) ),
 		);
 	}
 }
