5b2bad41a56e80fd857be2b4e5e07abb8e218854
Shesh Ghimire <sheshgh@outlook.com>
2025-04-14 18:41:55 +0545

    remove: column marshaller column name check. Handled by column names & offset.

--- Src/Marshaller/TableColumnMarshaller.php
+++ Src/Marshaller/TableColumnMarshaller.php
@@ -11,23 +11,16 @@ use TheWebSolver\Codegarage\Scraper\Interfaces\Transformer;
 /** @template-implements Transformer<TableTracer<string>,string> */
 class TableColumnMarshaller implements Transformer {
 	public function transform( string|array|DOMElement $element, object $scope ): string {
-		return ! $this->mappableColumnNameExists( $scope ) ? '' : trim(
-			match ( true ) {
-				$element instanceof DOMElement => $element->textContent,
-				is_string( $element )          => $element,
-				default                        => is_string( $contentPart = ( $element[3] ?? null ) )
-					? $contentPart
-					: throw new InvalidSource(
-						sprintf( '"%s" only supports normalized Table Column array.', self::class )
-					)
-			}
-		);
-	}
+		$transformed = match ( true ) {
+			$element instanceof DOMElement => $element->textContent,
+			is_string( $element )          => $element,
+			default                        => is_string( $contentPart = ( $element[3] ?? null ) )
+				? $contentPart
+				: throw new InvalidSource(
+					sprintf( '"%s" only supports normalized Table Column array.', self::class )
+				)
+		};
 
-	/** @param TableTracer<string> $scope */
-	private function mappableColumnNameExists( TableTracer $scope ): bool {
-		return ! ( $columnNames = $scope->getColumnNames() )
-			|| ! ( $columnName = $scope->getCurrentColumnName() )
-			|| in_array( $columnName, $columnNames, strict: true );
+		return trim( $transformed );
 	}
 }
