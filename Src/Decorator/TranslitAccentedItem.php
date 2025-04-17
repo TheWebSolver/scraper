751cb241e2198fde2bb88332395bedfab89e9ca9
Shesh Ghimire <sheshgh@outlook.com>
2025-04-17 14:15:43 +0545

    new: `TranslitAccentedItem` transformer  decorator.

---
+++ Src/Decorator/TranslitAccentedItem.php
@@ -0,0 +1,37 @@
+<?php
+declare( strict_types = 1 );
+
+namespace TheWebSolver\Codegarage\Scraper\Decorator;
+
+use DOMElement;
+use TheWebSolver\Codegarage\Scraper\Interfaces\Transformer;
+use TheWebSolver\Codegarage\Scraper\Interfaces\AccentedCharacter;
+
+/**
+ * @template TScope of AccentedCharacter
+ * @template-implements Transformer<TScope,string>
+ */
+class TranslitAccentedItem implements Transformer {
+	/** @param Transformer<contravariant TScope,string> $base */
+	public function __construct( private readonly Transformer $base ) {}
+
+	public function transform( string|array|DOMElement $element, object $scope ): string {
+		$characters = $scope->getDiacriticsList();
+
+		if ( ! $this->shouldTranslit( $scope, $characters ) ) {
+			return $this->base->transform( $element, $scope );
+		}
+
+		return ( $content = $this->base->transform( $element, $scope ) )
+			? str_replace( array_keys( $characters ), array_values( $characters ), $content )
+			: $content;
+	}
+
+	/**
+	 * @param array<string,string> $characters
+	 * @param TScope               $scope
+	 */
+	protected function shouldTranslit( object $scope, array $characters ): bool {
+		return ( $scope::ACTION_TRANSLIT === $scope->getAccentOperationType() ) && ! empty( $characters );
+	}
+}
