331ffe1c370bcd7c55275f1c30f0ead5fd2b0d11
Shesh Ghimire <sheshgh@outlook.com>
2025-10-26 22:17:27 +0545

    add: `FileFormat::getWriter()` method to get writer instance.

--- Src/Enums/FileFormat.php
+++ Src/Enums/FileFormat.php
@@ -3,7 +3,23 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Scraper\Enums;
 
+use InvalidArgumentException;
+use TheWebSolver\Codegarage\Scraper\Helper\JsonWriter;
+use TheWebSolver\Codegarage\Scraper\Interfaces\Writable;
+
 enum FileFormat: string {
 	case Json = 'json';
 	case Csv  = 'csv';
+
+	/**
+	 * @param iterable<array-key,TValue> $content
+	 * @throws InvalidArgumentException When unsupported format used.
+	 * @template TValue
+	 */
+	public function getWriter( iterable $content ): Writable {
+		return match ( $this ) {
+			default    => throw new InvalidArgumentException( "Unsupported format: {$this->value}" ),
+			self::Json => new JsonWriter( $content ),
+		};
+	}
 }
